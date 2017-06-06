5922fb78bd9d5faeff1463a602288654ce1ca9f7
a05h <a117q35mt@gmail.com>
2017-06-06 19:49:21 -0400

    socket server update

--- modules/socket.js
+++ modules/socket.js
@@ -3,12 +3,10 @@ module.exports = (app, server) => {
   var io = require('socket.io')(server);
   
   io.on('connection', (socket) => {
-    console.log('Connecting successful');
-    
-//    socket.on('disconnect', () => {
-//      console.log('Disconnected');
-//    });
-    
+    socket.on('message', (msg) => {
+      io.emit('message', msg);
+      //console.log('chat > ' + msg);
+    });
   });
   
 };
